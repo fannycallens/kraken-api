309ae6608e2811e8aa7e8aac0bbae486eef7123b
fannycallens <fannycallens@hotmail.com>
2021-12-03 16:02:45 +0000

    ws and rest api

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import ws_client
 import rest_api
-import json
+import asyncio
+
 
 def depth_callback(depthEvent):
+    #fs.recv()
     print(depthEvent)
+
+async def listen_for_message(websocket):
+    while True:
+        await asyncio.sleep(0)
+        try:
+            print('Listening for a message...')
+            message = await websocket.recv()
+            print("< {}".format(message))
+        except Exception as e:
+            print('Something happened')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -20,12 +32,16 @@ if __name__ == '__main__':
                                  'close[ordertype]': 'limit',
                                  'close[price]': '9001',
                                  'close[volume]': '1'})
+                                 "pair": [
+    "XBT/USD",
+    "XBT/EUR"
+  ],
     '''
 
-    subscribe_depth = {'event' : 'subscribe', 'subscription' : {'name' : 'depth'}}
-    subscribe_depth_json = json.dumps(subscribe_depth)
+    subscribe_depth = {'name' : 'depth'}
 
-    ws_client.WssClient().subscribe_public(subscribe_depth_json,depth_callback)
+    depthWs = ws_client.WssClient().subscribe_public(subscribe_depth, depth_callback)
+
 
     # to retrieve token for authentication
     # https://api.kraken.com/0/private/GetWebSocketsToken
@@ -41,4 +57,13 @@ if __name__ == '__main__':
     }
     '''
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# ssh-keygen -t rsa -b 4096 -C "fannycallens@hotmail.com"
+# ssh-add -K /Users/renauddrappier/.ssh/id_rsa
+
+#git remote add origin git@github.com:fannycallens/kraken-api.git
+#git remote set-url origin git@github.com:fannycallens/kraken-api.git
+#git branch -M main
+#git push -u origin main
+#git push --set-upstream git@github.com:fannycallens/kraken-api.git master
+#git remote set-url origin https://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@github.com/fannycallens/kraken-api.git
+#git push https://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@github.com/fannycallens/kraken-api.git
